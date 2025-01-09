6eb278002d5b0bee8e41a24f246c18a1032dcaed
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2025-01-09 16:34:30 -0330

    new: add GitHub and LinkedIn URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Weather App</h1>;
+  return (
+    <>
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/angelika-dar/" target="_blank">
+            Angelika Dar
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/angelika-dar/react-weather-app"
+            target="_blank"
+          >
+            open-source on GitHub
+          </a>
+        </p>
+      </footer>
+    </>
+  );
 }
