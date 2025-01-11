c5429af6fa931dcbcafc9550c68283a6f164faf4
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2025-01-10 21:23:33 -0330

    new: add Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,25 @@ import "./App.css";
 
 export default function App() {
   return (
-    <>
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a href="https://www.linkedin.com/in/angelika-dar/" target="_blank">
-            Angelika Dar
-          </a>{" "}
-          and it is{" "}
-          <a
-            href="https://github.com/angelika-dar/react-weather-app"
-            target="_blank"
-          >
-            open-source on GitHub
-          </a>
-        </p>
-      </footer>
-    </>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a href="https://www.linkedin.com/in/angelika-dar/" target="_blank">
+              Angelika Dar
+            </a>{" "}
+            and it is{" "}
+            <a
+              href="https://github.com/angelika-dar/react-weather-app"
+              target="_blank"
+            >
+              open-source on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
+    </div>
   );
 }
