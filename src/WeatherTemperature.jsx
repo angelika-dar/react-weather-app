e94b6f8c1d6669f60625e57f02df4ff64ce0e139
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2025-01-22 16:14:12 -0330

    modify: remove unneeded files

--- src/WeatherTemperature.jsx
+++ src/WeatherTemperature.jsx
@@ -1,47 +1,11 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 export default function WeatherTemperature({ celsius }) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(e) {
-    e.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(e) {
-    e.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(celsius)}</span>
-        <span className="unit">
-          C° |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            F°
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit())}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelsius}>
-            C°
-          </a>{" "}
-          | F°
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(celsius)}</span>
+      <span className="unit">C°</span>
+    </div>
+  );
 }
 
 // PropTypes validation
